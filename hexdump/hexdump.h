5028314acaaf428ffbb188b170feb35b2bbd729b
Wenbin Zheng <wbzheng@grandstream.cn>
2025-06-16 17:07:31 +0800

    add hexdump

---
+++ hexdump/hexdump.h
@@ -0,0 +1,10 @@
+#ifndef __HEXDUMP_H__
+#define __HEXDUMP_H__
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+void hexdump (const void *data, unsigned int size);
+
+#endif /* __HEXDUMP_H__ */
